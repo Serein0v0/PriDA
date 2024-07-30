a37ce12bebfccf7c2e951a16370af71187554779
Serein0v0 <39426230+Serein0v0@users.noreply.github.com>
2024-07-30 17:54:49 +0800

    Encryption.cpp

--- Encryption.cpp
+++ Encryption.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
-#include "/usr/local/include/pbc/pbc.h"  //必须包含头文件pbc.h#include <pbc/pbc.h>
+#include "/usr/local/include/pbc/pbc.h"  
 //#include<time.h> 
 #include <chrono>
 int main() {
 //clock_t startTime,endTime;
 
   pairing_t pairing;  
-  /*
-  pairing_t定义： pairings where elements belong本例子用a.param去初始化pairing; a pairing is a map 例如:e:G1×G2->Gt
-  a.param是pbc库中标准的参数集，其提供对称的pairing 
-  */
+
   char param[1024];
-  size_t count = fread(param, 1, 1024, stdin);//读大小为1的个数最大为1024到param，返回真实读入的个数
+  size_t count = fread(param, 1, 1024, stdin);
   printf("param = %s\n", param);
-//  printf("count = %lu\n", count);//size_t是标准C库中定义的，应为unsigned int，在64位系统中为 long unsigned int
+
   if (!count) pbc_die("input error");
   pairing_init_set_buf(pairing, param, count);
-  //pairing 初始化结束
 
-  //定义变量-初始化-用于保存系统参数、公私秘钥以及其他变量
   element_t u[5];
-  element_t M[5][3]; //收到的密钥中的M
+  element_t M[5][3]; 
   element_t g, h; 
   element_t delta[3];
   element_t C0[3];
@@ -44,7 +39,7 @@ int main() {
 
   element_init_G1(g, pairing);
   element_init_G2(h, pairing);//用pairing 初始化g
-  //初始化B
+
   for (int i=0; i<10; i++){
    element_init_Zr(b1[i], pairing);
    element_init_Zr(b2[i], pairing);
@@ -85,7 +80,7 @@ int main() {
   //element_init_GT(temp1, pairing);
   //element_init_GT(temp2, pairing);
 
-//随机生成矩阵B
+
   for (int i=0; i<10; i++){
   element_random(b1[i]);
   element_random(b2[i]); 
@@ -110,7 +105,7 @@ int main() {
     element_init_G1(Usr[a], pairing);
  }
 
-//计时开始
+
 
 
  // Start measuring time
@@ -119,7 +114,7 @@ int main() {
 //enc C0s
  auto begin = std::chrono::high_resolution_clock::now();
 for (int j=0; j<3; j++){
-  element_random(delta[j]);   //随机采样
+  element_random(delta[j]);   
   element_pow_zn( C0[j], g, delta[j]) ; 
 }
 
